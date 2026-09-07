651b1fb753a6fcf302893167d87d62c094c5d1b9
Meenakshi Sundaram <nmeenakshisundaram257@gmail.com>
2026-09-07 14:47:12 +0530

    chore: initialize Go module

---
+++ cmd/manager/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	log.SetFlags(0)
+	fmt.Println("k8s-custom-autoscaler: skeleton entrypoint running")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/githubnms/k8s-custom-autoscaler
+
+go 1.27.1
