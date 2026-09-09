7f8741409ad961943b946774966433788c9bfbee
Meenakshi Sundaram <nmeenakshisundaram257@gmail.com>
2026-09-09 19:54:37 +0530

    feat: bootstrap controller-runtime manager in main.go

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -1,11 +1,53 @@
 package main
 
 import (
-	"fmt"
-	"log"
+	"os"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	autoscalingv1alpha1 "github.com/githubnms/k8s-custom-autoscaler/api/v1alpha1"
+	"github.com/githubnms/k8s-custom-autoscaler/controllers"
 )
 
+var (
+	scheme = runtime.NewScheme()
+)
+
+func init() {
+	// Register built-in Kubernetes types (Deployment, Pod, etc.)
+	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+	// Register OUR custom type (ScalingPolicy) into the same scheme.
+	utilruntime.Must(autoscalingv1alpha1.AddToScheme(scheme))
+}
+
 func main() {
-	log.SetFlags(0)
-	fmt.Println("k8s-custom-autoscaler: skeleton entrypoint running")
+	ctrl.SetLogger(zap.New(zap.UseDevMode(true)))
+	setupLog := ctrl.Log.WithName("setup")
+
+	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+		Scheme: scheme,
+	})
+	if err != nil {
+		setupLog.Error(err, "unable to start manager")
+		os.Exit(1)
+	}
+
+	reconciler := &controllers.ScalingPolicyReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}
+	if err := reconciler.SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "ScalingPolicy")
+		os.Exit(1)
+	}
+
+	setupLog.Info("starting manager")
+	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+		setupLog.Error(err, "problem running manager")
+		os.Exit(1)
+	}
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.27.1
 require (
 	k8s.io/api v0.37.0
 	k8s.io/apimachinery v0.37.0
+	k8s.io/client-go v0.37.0
 	sigs.k8s.io/controller-runtime v0.25.0
 )
 
@@ -17,6 +18,7 @@ require (
 	github.com/fsnotify/fsnotify v1.9.0 // indirect
 	github.com/fxamacker/cbor/v2 v2.9.1 // indirect
 	github.com/go-logr/logr v1.4.3 // indirect
+	github.com/go-logr/zapr v1.3.0 // indirect
 	github.com/go-openapi/jsonpointer v1.0.0 // indirect
 	github.com/go-openapi/jsonreference v1.0.0 // indirect
 	github.com/go-openapi/swag v0.27.1 // indirect
@@ -44,6 +46,8 @@ require (
 	github.com/prometheus/procfs v0.21.1 // indirect
 	github.com/spf13/pflag v1.0.10 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.1 // indirect
 	go.yaml.in/yaml/v2 v2.4.4 // indirect
 	go.yaml.in/yaml/v3 v3.0.4 // indirect
 	golang.org/x/net v0.57.0 // indirect
@@ -58,7 +62,6 @@ require (
 	gopkg.in/evanphx/json-patch.v4 v4.13.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	k8s.io/apiextensions-apiserver v0.37.0 // indirect
-	k8s.io/client-go v0.37.0 // indirect
 	k8s.io/klog/v2 v2.140.0 // indirect
 	k8s.io/kube-openapi v0.0.0-20260721132016-d427ff9ee9ad // indirect
 	k8s.io/utils v0.0.0-20260626114624-be93311217bd // indirect
